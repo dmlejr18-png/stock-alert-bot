380476a23f1bdccc1d3cc4bb5e5aff515669161e
dmlejr18-png <dmlejr18@gmail.com>
2026-09-05 15:20:49 +0900

    Update oauth_server.py

--- oauth_server.py
+++ oauth_server.py
@@ -4,9 +4,9 @@ import requests
 app = Flask(__name__)
 
 # 카카오 디벨로퍼스에서 발급받은 정보 입력
-REST_API_KEY = "여기에_REST_API_KEY_입력"
-CLIENT_SECRET = "여기에_CLIENT_SECRET_입력"  # 보안 설정 안 썼으면 빈값 "" 가능
-REDIRECT_URI = "https://localhost:3000/redirect"  # 설정한 Redirect URI 주소
+REST_API_KEY = "52a818f0d25965b7e96a959adcbe57a0"
+CLIENT_SECRET = "xUBMdgzDOoL85JcnY0fB8RlGPbavAsGf"  # 보안 설정 안 썼으면 빈값 "" 가능
+REDIRECT_URI = "https://ubiquitous-space-fishstick-gx7rvg9g64x43vvgq-4000.app.github.dev/redirect"  # 설정한 Redirect URI 주소
 
 @app.route("/redirect")
 def redirect():
